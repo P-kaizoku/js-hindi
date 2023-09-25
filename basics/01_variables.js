07117ba11c46a5a25afeacc72acd1992edde0742
programming-he-ro <pabitram322@gmail.com>
2023-09-25 21:55:31 +0530

    variables and datatypes

---
+++ basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 144552
+let accountEmail = "pabitra@google.com"
+var accountPassword = "12345"
+accountCity = "Delhi"
+
+accountId = 2
+
+console.log(accountId);
---
+++ basics/02_datatypes.js
@@ -0,0 +1,8 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Hello") // we are using NodeJS not browser. Thats y its giving error
+
+console.log(typeof "pabitra")
+console.log(typeof null)// object
+console.log(typeof undefined)// undefined
+
